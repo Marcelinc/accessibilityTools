c28ab147f5a0b3ed6032fbff5c215b5ef2413ef2
Marcelinc <marcin.c40@wp.pl>
2023-01-21 10:53:53 +0100

    added components

---
+++ src/pages/UniversiteOne.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const UniversiteOne = () => {
+  return (
+    <div>UniversiteOne</div>
+  )
+}
+
+export default UniversiteOne
---
+++ src/pages/UniversityTwo.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const UniversityTwo = () => {
+  return (
+    <div>UniversityTwo</div>
+  )
+}
+
+export default UniversityTwo
